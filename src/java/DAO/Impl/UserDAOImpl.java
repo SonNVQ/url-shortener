212921daa094449fe7d4d3c976956e5ff037c2cf
Nguyen Son <50987070+SonNVQ@users.noreply.github.com>
2023-06-10 23:26:04 +0700

    Add DAOImpl
    Role, User

---
+++ src/java/DAO/Impl/UserDAOImpl.java
@@ -0,0 +1,232 @@
+package DAO.Impl;
+
+import DAO.DBContext;
+import DAO.DBContextImpl;
+import DAO.RoleDAO;
+import DAO.UserDAO;
+import Models.Role;
+import Models.User;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author nguyenson
+ */
+public class UserDAOImpl implements UserDAO {
+
+    DBContext dbContext;
+    RoleDAO roleDAO;
+
+    public UserDAOImpl() {
+        this.dbContext = new DBContextImpl();
+        this.roleDAO = new RoleDAOImpl();
+    }
+
+    @Override
+    public User addUser(User user) {
+        String sql = "insert into users(username, password, first_name, last_name, email, google_email) values (?, ?, ?, ?, ?, ?)";
+        try ( Connection cn = dbContext.getConnection();
+                 PreparedStatement ps = cn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            ps.setNString(1, user.getUsername());
+            ps.setNString(2, user.getPassword());
+            ps.setNString(3, user.getFirstName());
+            ps.setNString(4, user.getLastName());
+            ps.setNString(5, user.getEmail());
+            ps.setNString(6, user.getGoogleEmail());
+            int affectedRow = ps.executeUpdate();
+            if (affectedRow > 0) {
+                try ( ResultSet rs = ps.getGeneratedKeys()) {
+                    user.setPassword(null);
+                    user.setId(rs.getInt("id"));
+                }
+                return user;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public User getUserById(int id) {
+        String sql = "select username, password, first_name, last_name, email, google_email from users where id = ?";
+        try ( Connection cn = dbContext.getConnection();
+                 PreparedStatement ps = cn.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            try ( ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    User user = User.builder().id(rs.getInt("id"))
+                            .username(rs.getNString("username"))
+                            .firstName(rs.getNString("username"))
+                            .lastName(rs.getNString("last_name"))
+                            .email(rs.getNString("email"))
+                            .googleEmail(rs.getNString("google_email"))
+                            .build();
+                    return user;
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        String sql = "select username, password, first_name, last_name, email, google_email from users where username = ?";
+        try ( Connection cn = dbContext.getConnection();
+                 PreparedStatement ps = cn.prepareStatement(sql)) {
+            ps.setNString(1, username);
+            try ( ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    User user = User.builder().id(rs.getInt("id"))
+                            .username(rs.getNString("username"))
+                            .firstName(rs.getNString("username"))
+                            .lastName(rs.getNString("last_name"))
+                            .email(rs.getNString("email"))
+                            .googleEmail(rs.getNString("google_email"))
+                            .build();
+                    return user;
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public User login(String username, String password) {
+        String sql = "select username, password, first_name, last_name, email, google_email from users where username = ?, password = ?";
+        try ( Connection cn = dbContext.getConnection();
+                 PreparedStatement ps = cn.prepareStatement(sql)) {
+            ps.setNString(1, username);
+            ps.setNString(2, password);
+            try ( ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    User user = User.builder().id(rs.getInt("id"))
+                            .username(rs.getNString("username"))
+                            .firstName(rs.getNString("username"))
+                            .lastName(rs.getNString("last_name"))
+                            .email(rs.getNString("email"))
+                            .googleEmail(rs.getNString("google_email"))
+                            .build();
+                    return user;
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public User updateUser(User user) {
+        String sql = "update users set username = ?, password = ?, first_name = ?, last_name = ?, email = ?, google_email = ? from users where id = ?";
+        try ( Connection cn = dbContext.getConnection();
+                 PreparedStatement ps = cn.prepareStatement(sql)) {
+            ps.setNString(1, user.getUsername());
+            ps.setNString(2, user.getPassword());
+            ps.setNString(3, user.getFirstName());
+            ps.setNString(4, user.getLastName());
+            ps.setNString(5, user.getEmail());
+            ps.setNString(6, user.getGoogleEmail());
+            int affectedRow = ps.executeUpdate();
+            if (affectedRow > 0) {
+                return user;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public User deleteUser(User user) {
+        String sql = "delete from users where id = ?";
+        try ( Connection cn = dbContext.getConnection();
+                 PreparedStatement ps = cn.prepareStatement(sql)) {
+            ps.setInt(1, user.getId());
+            int affectedRow = ps.executeUpdate();
+            if (affectedRow > 0) {
+                return user;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public User addUserRole(User user, Role role) {
+        Integer roleID = roleDAO.getRoleIdByRoleName(role);
+        if (roleID == null) {
+            return null;
+        }
+        String sql = "insert into user_role values (?, ?)";
+        try ( Connection cn = dbContext.getConnection();
+                 PreparedStatement ps = cn.prepareStatement(sql)) {
+            ps.setInt(1, user.getId());
+            int affectedRow = ps.executeUpdate();
+            if (affectedRow > 0) {
+                return user;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public List<Role> getRoles(User user) {
+        String getRoleIDsSql = "select role_id from users where user_id = ?";
+        try ( Connection cn = dbContext.getConnection();
+                 PreparedStatement ps = cn.prepareStatement(getRoleIDsSql)) {
+            ps.setInt(1, user.getId());
+            try ( ResultSet rs = ps.executeQuery()) {
+                List<Integer> roleList = new ArrayList<>();
+                while (rs.next()) {
+                    roleList.add(rs.getInt("role_id"));
+                }
+                List<Role> roles = new ArrayList<>();
+                roleList.forEach((Integer roleId) -> {
+                    roles.add(roleDAO.getRoleById(roleId));
+                });
+                return roles;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public User deleteUserRole(User user, Role role) {
+        Integer roleID = roleDAO.getRoleIdByRoleName(role);
+        if (roleID == null) {
+            return null;
+        }
+        String sql = "delete from user_role where user_id = ?, role_id = ?";
+        try ( Connection cn = dbContext.getConnection();
+                 PreparedStatement ps = cn.prepareStatement(sql)) {
+            ps.setInt(1, user.getId());
+            ps.setInt(2, roleID);
+            int affectedRow = ps.executeUpdate();
+            if (affectedRow > 0) {
+                return user;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+}
