7b0f0c425b601758b9ea3b03e8eee5ededebdfca
Nguyen Son <50987070+SonNVQ@users.noreply.github.com>
2023-06-11 00:53:55 +0700

    Add Constants
    Add REGEX

---
+++ src/java/Constants/Regex.java
@@ -0,0 +1,16 @@
+package Constants;
+
+/**
+ *
+ * @author nguyenson
+ */
+public class Regex {
+
+    public static final String USERNAME_CHECK = "";
+
+    public static final String PASSWORD_CHECK = "";
+
+    public static final String EMAIL_CHECK = "";
+
+    public static final String NON_EMPTY_STRING_CHECK = "";
+}
