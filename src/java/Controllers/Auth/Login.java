e3df516fab6de323745aae660fcd401dc37eb202
Nguyen Son <50987070+SonNVQ@users.noreply.github.com>
2023-06-11 00:54:14 +0700

    Add Auth Controllers

---
+++ src/java/Controllers/Auth/Login.java
@@ -0,0 +1,31 @@
+package Controllers.Auth;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author nguyenson
+ */
+@WebServlet(name="Login", urlPatterns={"/auth/login"})
+public class Login extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        
+    } 
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+    }
+
+}
---
+++ src/java/Controllers/Auth/Register.java
@@ -0,0 +1,30 @@
+package Controllers.Auth;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author nguyenson
+ */
+@WebServlet(name = "Register", urlPatterns = {"/auth/register"})
+public class Register extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+}
